f3b21c5c626aa223fa637356b736ae77952567cb
Elisha-Tuan <pokemenggou0.0@gmail.com>
2022-06-10 17:52:22 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f https://github.com/ALPHACamp/forum-front-end-vue.git main:gh-pages
+git push -f https://github.com/Kenneth-Tuan/Kenneth-s-portfolio.git main:gh-pages
 
 cd -
