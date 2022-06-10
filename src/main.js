97f2726e5537b8d2bc1e1beb34b8341db9cdf05f
Elisha-Tuan <pokemenggou0.0@gmail.com>
2022-06-10 17:45:22 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import VueCompositionAPI from '@vue/composition-api'
+import { MotionPlugin } from '@vueuse/motion'
+
+
+Vue.use(MotionPlugin)
+Vue.use(VueCompositionAPI)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
