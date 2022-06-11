122a764cee6f38bd9333a707787ea787946d728b
Elisha-Tuan <pokemenggou0.0@gmail.com>
2022-06-11 18:42:37 +0800

    feat: Kenneth to kenneth

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? './Kenneth-s-portfolio/'
+        ? './kenneth-s-portfolio/'
         : './',
     css: { extract: false }
 }
